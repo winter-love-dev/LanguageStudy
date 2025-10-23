9809980692d47bfce059a60b23a992b873d2e0eb
winter-love-dev <winter.kim@mvlchain.io>
2025-10-24 08:39:45 +0900

    Setup design-system gradle

--- core-android/design-system/build.gradle.kts
+++ core-android/design-system/build.gradle.kts
@@ -1,40 +1,10 @@
+import dev.love.winter.convention.extension.setNamespace
+
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    id("winter.android.library")
+    id("winter.compose.library")
 }
 
 android {
-    namespace = "dev.love.winter.designsystem"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-}
-
-dependencies {
-
-    implementation(libs.androidX.coreKtx)
-    implementation(libs.androidX.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidX.testJunit)
-    androidTestImplementation(libs.androidX.testEspresso)
+    setNamespace("designsystem")
 }
