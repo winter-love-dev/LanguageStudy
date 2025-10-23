916a5904f800c422f32f6a9493f80bbc77307e1c
winter-love-dev <winter.kim@mvlchain.io>
2025-10-24 08:40:42 +0900

    Add color token

---
+++ core-android/design-system/src/main/java/dev/love/winter/designsystem/theme/Color.kt
@@ -0,0 +1,63 @@
+package dev.love.winter.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Brand - Primary
+val Primary50 = Color(0xFFF1F1FF)
+val Primary100 = Color(0xFFDBDFFF)
+val Primary200 = Color(0xFFC1C8FF)
+val Primary300 = Color(0xFF9397FF)
+val Primary400 = Color(0xFF7278FF)
+val Primary500 = Color(0xFF5653FF)
+val Primary600 = Color(0xFF4745E4)
+val Primary700 = Color(0xFF3B39AF)
+val Primary800 = Color(0xFF2B2A85)
+val Primary900 = Color(0xFF131243)
+
+// Neutral - Grey
+val Grey50 = Color(0xFFFDFDFD)
+val Grey100 = Color(0xFFF4F4F5)
+val Grey200 = Color(0xFFE7E7EA)
+val Grey300 = Color(0xFFD9D9DC)
+val Grey400 = Color(0xFFBFC0C9)
+val Grey500 = Color(0xFF8D8F9B)
+val Grey600 = Color(0xFF60626C)
+val Grey700 = Color(0xFF484951)
+val Grey800 = Color(0xFF313237)
+val Grey900 = Color(0xFF18181B)
+
+// Semantic - Green
+val Green50 = Color(0xFFECFAF3)
+val Green100 = Color(0xFFC3EEDB)
+val Green200 = Color(0xFFA5E6CA)
+val Green300 = Color(0xFF7CDAB1)
+val Green400 = Color(0xFF63D3A2)
+val Green500 = Color(0xFF3CC88B)
+val Green600 = Color(0xFF37B67E)
+val Green700 = Color(0xFF247E57)
+val Green800 = Color(0xFF115B3A)
+val Green900 = Color(0xFF0F3122)
+
+// Semantic - Yellow
+val Yellow50 = Color(0xFFFFF7E9)
+val Yellow100 = Color(0xFFFFEAC7)
+val Yellow200 = Color(0xFFFFDA9A)
+val Yellow300 = Color(0xFFFFCD78)
+val Yellow400 = Color(0xFFFFBD49)
+val Yellow500 = Color(0xFFFFAF24)
+val Yellow600 = Color(0xFFF2A522)
+val Yellow700 = Color(0xFFC7881A)
+val Yellow800 = Color(0xFF825911)
+val Yellow900 = Color(0xFF4A340E)
+
+// Semantic - Red
+val Red50 = Color(0xFFFCEBEB)
+val Red100 = Color(0xFFFFD7D7)
+val Red200 = Color(0xFFF2A1A1)
+val Red300 = Color(0xFFEC7676)
+val Red400 = Color(0xFFE85B5B)
+val Red500 = Color(0xFFE23232)
+val Red600 = Color(0xFFCE2E2E)
+val Red700 = Color(0xFFA02424)
+val Red800 = Color(0xFF6C1A1A)
+val Red900 = Color(0xFF401111)
