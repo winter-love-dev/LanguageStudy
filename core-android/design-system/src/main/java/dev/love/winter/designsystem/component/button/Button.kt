29bd32e6ac413ee2d4d6543de32c50e8d0c9eccf
winter-love-dev <winter.kim@mvlchain.io>
2025-10-31 10:37:12 +0900

    Fix lint

--- core-android/design-system/src/main/java/dev/love/winter/designsystem/component/button/Button.kt
+++ core-android/design-system/src/main/java/dev/love/winter/designsystem/component/button/Button.kt
@@ -20,11 +20,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.stateDescription
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.love.winter.designsystem.component.button.spec.ButtonColors
