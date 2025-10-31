29dd6ce3432fcaac9a9a13af7d9d01dd8d9a4aca
winter-love-dev <winter.kim@mvlchain.io>
2025-10-31 10:38:17 +0900

    Add button preview example

--- sample/design-system-catalog/src/main/java/dev/love/winter/dscatalog/catalog/button/Screen.kt
+++ sample/design-system-catalog/src/main/java/dev/love/winter/dscatalog/catalog/button/Screen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
@@ -14,7 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.love.winter.designsystem.component.button.Button
+import dev.love.winter.designsystem.component.button.spec.ButtonIcon
 import dev.love.winter.designsystem.component.button.spec.ButtonShape
 import dev.love.winter.designsystem.component.button.spec.ButtonState
 import dev.love.winter.designsystem.component.button.spec.ButtonType
@@ -41,13 +44,92 @@ fun ButtonScreen(
                 vertical = WinterTheme.spacing.extraExtraLarge,
             ),
     ) {
-        listOf(
-            ButtonType.Primary,
-            ButtonType.Secondary,
+        TopSection()
+        BottomSection()
+    }
+}
+
+/**
+ * - Summary
+ * - Primary
+ * - Secondary
+ * - Tertiary
+ */
+@Composable
+fun TopSection(
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = "Summary",
+            style = WinterTheme.typography.titleLarge,
+            color = WinterTheme.color.textBody,
+        )
+        Row(
+            Modifier
+                .background(color = WinterTheme.color.background)
+                .padding(top = WinterTheme.spacing.medium)
+                .width(350.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Button(
+                label = "Button",
+                onPress = {},
+                shape = ButtonShape.Small,
+            )
+            Button(
+                label = "Button",
+                onPress = {},
+                icon = ButtonIcon.Leading(
+                    resource = WinterTheme.icon.input.plus.outlined,
+                    contentDescription = "Settings",
+                )
+            )
+            Button(
+                label = "Button",
+                onPress = {},
+                shape = ButtonShape.Large,
+                icon = ButtonIcon.Trailing(
+                    resource = WinterTheme.icon.input.plus.outlined,
+                    contentDescription = "Settings",
+                )
+            )
+        }
+        Divider()
+        ButtonTypeSection(
+            buttonTypes = listOf(
+                ButtonType.Primary,
+                ButtonType.Secondary,
+            ),
+        )
+    }
+}
+
+/**
+ * - Brand
+ * - Critical
+ */
+@Composable
+private fun BottomSection(
+    modifier: Modifier = Modifier,
+) {
+    ButtonTypeSection(
+        modifier = modifier,
+        buttonTypes = listOf(
             ButtonType.Tertiary,
             ButtonType.Brand,
             ButtonType.Critical,
-        ).forEach { buttonType ->
+        ),
+    )
+}
+
+@Composable
+private fun ButtonTypeSection(
+    modifier: Modifier = Modifier,
+    buttonTypes: List<ButtonType>,
+) {
+    Column(modifier = modifier) {
+        buttonTypes.forEach { buttonType ->
             Text(
                 text = buttonType.toString(),
                 style = WinterTheme.typography.titleLarge,
@@ -67,11 +149,9 @@ fun ButtonScreen(
                     )
                 )
                 Row(
-                    modifier = Modifier
-                        .padding(
-                            vertical = WinterTheme.spacing.extraSmall,
-                            horizontal = WinterTheme.spacing.small,
-                        ),
+                    modifier = Modifier.padding(
+                        vertical = WinterTheme.spacing.extraSmall,
+                    ),
                     horizontalArrangement = Arrangement.spacedBy(WinterTheme.spacing.small),
                 ) {
                     Button(
@@ -96,6 +176,43 @@ fun ButtonScreen(
                         onPress = {},
                     )
                 }
+                Text(
+                    text = "$buttonShape - with image",
+                    style = WinterTheme.typography.titleSmall,
+                    color = WinterTheme.color.textBody,
+                    modifier = Modifier.padding(
+                        top = WinterTheme.spacing.small,
+                    )
+                )
+                Row(
+                    modifier = Modifier.padding(
+                        vertical = WinterTheme.spacing.extraSmall,
+                    ),
+                    horizontalArrangement = Arrangement.spacedBy(WinterTheme.spacing.small),
+                ) {
+                    Button(
+                        label = "Leading",
+                        type = buttonType,
+                        state = ButtonState.Default,
+                        shape = buttonShape,
+                        icon = ButtonIcon.Leading(
+                            resource = WinterTheme.icon.input.plus.outlined,
+                            contentDescription = "Settings",
+                        ),
+                        onPress = {},
+                    )
+                    Button(
+                        label = "Trailing",
+                        type = buttonType,
+                        state = ButtonState.Default,
+                        shape = buttonShape,
+                        icon = ButtonIcon.Trailing(
+                            resource = WinterTheme.icon.input.plus.outlined,
+                            contentDescription = "Settings",
+                        ),
+                        onPress = {},
+                    )
+                }
             }
             Divider()
         }
@@ -116,20 +233,52 @@ private fun Divider() {
 @Preview(
     name = "Light",
     uiMode = Configuration.UI_MODE_NIGHT_NO,
-    heightDp = 1800,
+    heightDp = 1450,
 )
 @Preview(
     name = "Dark",
     uiMode = Configuration.UI_MODE_NIGHT_YES,
-    heightDp = 1800,
+    heightDp = 1450,
 )
 @Composable
-private fun Preview() {
+private fun Preview1() {
     WinterTheme {
         Box(
             Modifier.background(WinterTheme.color.background)
         ) {
-            ButtonScreen()
+            TopSection(
+                modifier = Modifier.padding(
+                    horizontal = WinterTheme.spacing.small,
+                    vertical = WinterTheme.spacing.extraExtraLarge,
+                ),
+            )
         }
     }
 }
+
+@Preview(
+    name = "Light",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    heightDp = 1900,
+)
+@Preview(
+    name = "Dark",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    heightDp = 1900,
+)
+@Composable
+private fun Preview2() {
+    WinterTheme {
+        Box(
+            Modifier.background(WinterTheme.color.background)
+        ) {
+            BottomSection(
+                modifier = Modifier.padding(
+                    horizontal = WinterTheme.spacing.small,
+                    vertical = WinterTheme.spacing.extraExtraLarge,
+                ),
+            )
+        }
+    }
+}
+
