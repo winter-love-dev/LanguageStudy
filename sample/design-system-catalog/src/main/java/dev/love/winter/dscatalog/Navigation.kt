eb7129cbbb7ec47657efa3a65948b4ee0d0f6e65
winter-love-dev <winter.kim@mvlchain.io>
2025-11-01 20:04:52 +0900

    Fix lint

--- sample/design-system-catalog/src/main/java/dev/love/winter/dscatalog/Navigation.kt
+++ sample/design-system-catalog/src/main/java/dev/love/winter/dscatalog/Navigation.kt
@@ -69,9 +69,7 @@ fun Navigation(
                 ) {
                     BorderRadiusRoute()
                 }
-                entry<ColorsScreen>(
-
-                ) {
+                entry<ColorsScreen> {
                     ColorRoute()
                 }
                 entry<IconsScreen> {
