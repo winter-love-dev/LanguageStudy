9b424af56c91f94ee5193393e0815793c297d21e
winter-love-dev <winter.kim@mvlchain.io>
2025-11-02 12:30:34 +0900

    Add appCompatActivity()

--- sample/design-system-catalog/src/main/java/dev/love/winter/dscatalog/MainActivity.kt
+++ sample/design-system-catalog/src/main/java/dev/love/winter/dscatalog/MainActivity.kt
@@ -1,14 +1,14 @@
 package dev.love.winter.dscatalog
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import dev.love.winter.designsystem.theme.WinterTheme
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
